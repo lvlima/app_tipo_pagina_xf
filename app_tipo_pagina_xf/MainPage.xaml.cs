e585ad1c02a742cc65bb6bf621278dcfbfb23021
lvlima <luizvictor.al@gmail.com>
2018-06-25 10:59:00 -0300

    Commit do app de tipos de páginas

---
+++ app_tipo_pagina_xf/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace app_tipo_pagina_xf
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
